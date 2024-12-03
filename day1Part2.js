5922c810a943ec79177b89be44a1c18751e4fc78
Andrew Pearson <andrewtpearson94@gmail.com>
2024-12-03 12:02:04 +0000

    Feat: solved day 1 problem 2

--- day1Part2.js
+++ day1Part2.js
@@ -5,9 +5,13 @@ const [leftList, rightList] = getSortedDayOneInputs();
 let similarityScore = 0;
 
 for (let i = 0; i < leftList.length; i++) {
-  for (let j = 0; j < rightList.length; j++) {
 
+  let occurrences = 0;
+  for (let j = 0; j < rightList.length; j++) {
+    if (rightList[j] === leftList[i]) occurrences++;
+    if (rightList[j] > leftList[i]) break;
   }
+  similarityScore += (leftList[i] * occurrences);
 }
 
-console.log(similarityScore);
+console.log('final similarity score is', similarityScore);
